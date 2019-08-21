1923fbbe1a400cd0d5aeb79bece8c0171494bd00
Aries <aries@asterism.xyz>
2019-08-21 11:23:04 +0900

    Update pl-update.sh

--- pleroma/script/pl-update.sh
+++ pleroma/script/pl-update.sh
@@ -5,3 +5,4 @@ sudo docker-compose -f ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.manage.yml pull
 sudo docker-compose -f ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.manage.yml run --rm web mix ecto.migrate
 sudo docker stack deploy pleroma -c ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.yml
 sudo docker system prune --force
+sudo docker node ls |grep Ready|awk '$2{print$2}'|grep -v "*"|xargs -L 1 -I {} ssh user@{} -i ~/key.pem "sudo docker system prune --force"
