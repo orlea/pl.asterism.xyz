6b9df6547fa9af7ebc33acd33d386f1ef6055727
aries <aries@asterism.xyz>
2020-01-05 10:51:30 +0900

    Change build method to multistage build

--- pleroma/script/pl-update.sh
+++ pleroma/script/pl-update.sh
@@ -2,7 +2,7 @@
 set -uxe
 
 sudo docker-compose -f ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.manage.yml pull
-sudo docker-compose -f ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.manage.yml run --rm web ./release/bin/pleroma_ctl migrate
+sudo docker-compose -f ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.manage.yml run --rm web /opt/pleroma/bin/pleroma_ctl migrate
 sudo docker stack deploy pleroma -c ~/pl.asterism.xyz/pleroma/pl-asterism-xyz.yml
 sudo docker system prune --force
 sudo docker node ls |grep Ready|awk '$2{print$2}'|grep -v "*"|xargs -L 1 -I {} ssh user@{} -i ~/key.pem "sudo docker system prune --force"
